5dfb05cf8997b1d46eed4699ca76229437cf0644
Maxim Servulya <maxim.servulya@postindustria.com>
2012-07-18 16:47:13 +0300

    draw red background

--- src/org/andengine/examples/LiveWallpaperService.java
+++ src/org/andengine/examples/LiveWallpaperService.java
@@ -5,40 +5,42 @@ import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.scene.background.IBackground;
 import org.andengine.extension.ui.livewallpaper.BaseLiveWallpaperService;
 
+import android.util.Log;
 
 public class LiveWallpaperService extends BaseLiveWallpaperService {
 
 	private ScreenOrientation mScreenOrientation;
 	private static final int CAMERA_WIDTH = 512;
 	private static final int CAMERA_HEIGHT = 768;
-	
+
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		return new EngineOptions(true, this.mScreenOrientation, new FillResolutionPolicy(), new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT));
 	}
 
 	@Override
-	public void onCreateResources(
-			OnCreateResourcesCallback pOnCreateResourcesCallback)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception {
+		Log.i("AAAA", "onCreateResources");
+		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
 
 	@Override
-	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
+		Log.i("AAAA", "onCreateScene");
+		final Scene scene = new Scene(1);
+		IBackground b = new Background(1, 0, 0);
+		scene.setBackground(b);
+		pOnCreateSceneCallback.onCreateSceneFinished(scene);
 	}
 
 	@Override
-	public void onPopulateScene(Scene pScene,
-			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+		Log.i("AAAA", "onPopulateScene");
+		pOnPopulateSceneCallback.onPopulateSceneFinished();
 	}
 
 }
